9925b2b621f0341bd31115d8f97242a3a5e163bd
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-02-01 21:57:02 +0100

    add API post request

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
 //     textRating.innerText = `${rating}`;
 //   });
 // });
-
+// import 'dotenv/config'
+let rating = 0
 const textRating = document.querySelector(".rating__value");
 const stars = document.querySelectorAll(".rating__star");
 
@@ -21,6 +22,25 @@ stars.forEach((star, i) => {
     }
 
     // Set the rating value
-    textRating.innerText = `${i + 1}`;
+    rating = `${i + 1}`;
+    textRating.innerText = rating;
+
   });
 });
+
+
+const postData = async () => {
+
+  const myResponse = await fetch(`https://test-api.codingbootcamp.cz/api/${process.env.MY_API_KEY}/ratings`, {
+    "method": "POST",
+    "body": JSON.stringify({
+      "rating_subject": `Movie Number ${Math.floor(Math.random() * 50)}`,
+      "rating_value": rating
+    }),
+    "headers": {
+      'Content-Type': 'application/json'
+    }
+  })
+  const myUsableResponse = await myResponse.json()
+  console.log(myUsableResponse)
+}
